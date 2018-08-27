9ad3de71a328deaee962fb9db2b9d2656cabdbe3
qua <quamv@outlook.com>
2018-08-26 22:46:29 -0400

    Add project files.

---
+++ chinook_lib_netstandard_ef/Migrations/20180827024611_Initial.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace chinook_lib_netstandard_ef.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "media_types",
+                columns: table => new
+                {
+                    Media_TypeId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_media_types", x => x.Media_TypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "playlists",
+                columns: table => new
+                {
+                    PlaylistId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_playlists", x => x.PlaylistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "playlist_track",
+                columns: table => new
+                {
+                    PlaylistId = table.Column<int>(nullable: false),
+                    TrackId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_playlist_track", x => new { x.PlaylistId, x.TrackId });
+                    table.ForeignKey(
+                        name: "FK_playlist_track_playlists_PlaylistId",
+                        column: x => x.PlaylistId,
+                        principalTable: "playlists",
+                        principalColumn: "PlaylistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "genres");
+
+            migrationBuilder.DropTable(
+                name: "media_types");
+
+            migrationBuilder.DropTable(
+                name: "playlist_track");
+
+            migrationBuilder.DropTable(
+                name: "playlists");
+        }
+    }
+}
---
+++ chinook_lib_netstandard_ef/Model/MediaType.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace chinook_lib_netstandard_ef.Model
+{
+    public class ChinookDbContext : DbContext
+    {
+        const string data_source_string = "Data Source=chinook.db";
+
+        public DbSet<media_type> media_types{ get; set; }
+        public DbSet<genre> genres { get; set; }
+        public DbSet<playlist> playlists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(data_source_string);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<playlist_track>()
+                .HasKey(c => new { c.PlaylistId, c.TrackId }); // c.FarmerSS, c.HotelID });
+        }
+    }
+
+    public class media_type
+    {
+        public int Media_TypeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+    public class genre
+    {
+        public int GenreId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }   
+    public class playlist
+    {
+        public int PlaylistId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<playlist_track> tracks { get; set; }
+    }
+    public class playlist_track
+    {
+        [Key]
+        [Column(Order=1)]
+        public int PlaylistId { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public int TrackId { get; set; }
+    }
+    //public class track
+    //{
+    //    public int TrackId;
+    //    public string Name;
+    //    public int AlbumId;
+    //    public int MediaTypeId;
+    //    public int GenreId;
+    //    public string Composer;
+    //    public int Milliseconds;
+    //    public int Bytes;
+    //    public float UnitPrice;
+    //}
+    //public class invoice
+    //{
+    //    public int InvoiceId;
+    //    public int CustomerId;
+    //    public DateTime InvoiceDate;
+    //    public string BillingAddress;
+    //    public string BillingCity;
+    //    public string BillingState;
+    //    public string BillingZip;
+    //}
+    //public class invoice_item
+    //{
+    //    public int InvoiceItemId;
+    //    public int InvoiceId;
+    //    public int TrackId;
+    //    public float UnitPrice;
+    //    public int Quantity;
+    //}
+    //public class album
+    //{
+    //    public int AlbumId;
+    //    public string Title;
+    //    public int ArtistId;
+    //}
+    //public class artist
+    //{
+    //    public int ArtistId;
+    //    public string Name;
+    //}
+    //public class customer
+    //{
+    //    public int CustomerId;
+    //    public string FirstName;
+    //    public string LastName;
+    //    public string Company;
+    //    public string Address;
+    //    public string City;
+    //    public string State;
+    //    public string Country;
+    //    public string PostalCode;
+    //    public string Phone;
+    //    public string Fax;
+    //    public string Email;
+    //    public int SupportRepId;
+    //}
+    //public class employee
+    //{
+    //    public int EmployeeId;
+    //    public string LastName;
+    //    public string FirstName;
+    //    public string Title;
+    //    public int ReportsTo;
+    //    public DateTime BirthDate;
+    //}
+
+}
