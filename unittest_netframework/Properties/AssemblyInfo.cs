e338f0b15717fa85c8bc4cd5601083e47e2de533
qua <quamv@outlook.com>
2018-08-27 22:05:08 -0400

    no libs added yet

---
+++ unittest_netframework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("unittest_netframework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unittest_netframework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb5be1c5-8cae-4712-98ee-affd1b1b29df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ unittest_netframework/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using chinook_lib_netstandard_ef.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unittest_netframework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var db = new ChinookDbContext();
+            var playlist = db.playlists.Include("tracks").FirstOrDefault();
+
+            if (playlist == null)
+            {
+                var new_track = new track() { TrackId = null, Name = "Bad Medicine" };
+                db.tracks.Add(new_track);
+
+                var new_playlist = new playlist() { PlaylistId = 0, Name = "Test playlist" };
+                db.playlists.Add(new_playlist);
+
+                var ref_playlist_track = new playlist_track() { playlist = new_playlist, track = new_track };
+                new_playlist.tracks.Add(ref_playlist_track);
+                db.SaveChanges();
+
+                Tracks.ItemsSource = new_playlist.tracks;
+            }
+            else
+            {
+                Tracks.ItemsSource = playlist.tracks; // db.playlists.Include("tracks").ToList();
+            }
+
+        }
+    }
